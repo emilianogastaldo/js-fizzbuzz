c21d559f0cf10d8d53b433adb5270a28f51bc567
emilianogastaldo <emiliano0303@gmail.com>
2023-12-20 15:50:36 +0100

    added for 1 to 100

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('JS OK')
+
+//Creo il ciclo per stampare i numeri
+
+for (let i = 1; i <= 100; i++) {
+    let flag = i;
+    if (i % 3 == 0 && i % 5 == 0) {
+        flag = 'BuzzFizz';
+    } else if (i % 3 == 0) {
+        flag = 'Fizz';
+    } else if (i % 5 == 0) {
+        flag = 'Buzz';
+    }
+    console.log('numero:', flag);
+
+}
