a5a2ceb8dfd5f5739293d6d55258bd1856b11c3c
emilianogastaldo <emiliano0303@gmail.com>
2023-12-20 16:10:33 +0100

    added list

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 console.log('JS OK')
 
-//Creo il ciclo per stampare i numeri
+//Recupero i tag che potrebbero servirmi
+const containerNum = document.querySelector('.container');
 
+
+//Creo il ciclo per stampare i numeri
+const rowNumber = document.createElement('div');
+rowNumber.classList.add('row', 'text-white');
 for (let i = 1; i <= 100; i++) {
+    const colNumber = document.createElement('div');
+    colNumber.classList.add('col');
     let flag = i;
     if (i % 15 == 0) {
         flag = 'BuzzFizz';
@@ -11,6 +18,9 @@ for (let i = 1; i <= 100; i++) {
     } else if (i % 5 == 0) {
         flag = 'Buzz';
     }
+    colNumber.innerText = flag;
     console.log('numero:', flag);
-
+    rowNumber.appendChild(colNumber);
 }
+
+containerNum.appendChild(rowNumber)
